c482ed5f68e24bc9a702550c33e4db775afb5729
Gary <jj3179@columbia.edu>
2023-02-22 21:49:23 -0500

    update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({ title, classProps }) =>
+
 const Navbar = () => {
     return(
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer"/>
+                <ul className="test-white md:flex hidden list-none flex-row justify-center items-center flex-initial">
+
+                </ul>
+            </div>
+        </nav>
     );
 }
 export default Navbar;
