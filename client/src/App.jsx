20ef18ddb9fc73a13b7b7db4f551d484cb80b63b
Gary <jj3179@columbia.edu>
2023-02-22 21:23:51 -0500

    initialized react app 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+const App = () => (
+    <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+            <Navbar />
+            <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
     </div>
-  )
-}
+);
 
-export default App
+export default App;
