e4bb256afc85de439741b1f1300b6a783ed8dc8f
Gary <jj3179@columbia.edu>
2023-02-26 00:00:59 -0500

    front end welcome page done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,19 +7,36 @@ import {BiSad} from "react-icons/all";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glass-morphism"
+    />
+);
+
 const Welcome = () => {
     const connectWallet = () => {
 
     }
+
+    const handleSubmit = () => {
+
+    }
+
+    let handleChange;
     return(
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className=" flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className=" flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                        Send Bosstime Coin <br /> across the world
+                        Send BOS <br /> across the world
                     </h1>
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on BOS.
                     </p>
                     <button
                         type="button"
@@ -66,8 +83,28 @@ const Welcome = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glass-morphism">
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                        <Input placeholder="Amount (ETH) " name="amount" type="number" handleChange={handleChange} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
+                        <div className="h-[1px] w-full bg-gray-400 my-2"/>
+                        {false
+                            ? <Loader />
+                            : (
+                                <button
+                                    type="button"
+                                    onClick={handleSubmit}
+                                    className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                                >
+                                    Send now
+                                </button>
+                            )}
+
+                    </div>
+
+                </div>
             </div>
         </div>
     );
