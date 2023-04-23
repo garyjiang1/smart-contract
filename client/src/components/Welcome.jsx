1bdf398039f7fd72ea42b7913c0e996b9da2831b
Gary <jj3179@columbia.edu>
2023-04-22 22:19:11 -0400

    update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -26,17 +26,17 @@ const Welcome = () => {
     const handleSubmit = () => {
 
     }
-
+    
     let handleChange;
     return(
         <div className="flex w-full justify-center items-center">
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className=" flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-                        Send BOS <br /> across the world
+                        Send Crypto <br /> across the world
                     </h1>
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-                        Explore the crypto world. Buy and sell cryptocurrencies easily on BOS.
+                        Explore the crypto world. Buy and sell cryptocurrencies on our service.
                     </p>
                     <button
                         type="button"
