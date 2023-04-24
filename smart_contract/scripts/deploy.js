052457ae9dc34a13d69001438e7ea11fe15ac0c7
Gary <jj3179@columbia.edu>
2023-04-24 17:00:16 -0400

    test

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,20 +1,20 @@
-const main = async() => {
- const Transactions  = await hre.ethers.getContractFactory("Transactions");
- const transactions = await Transactions.deploy();
+const main = async () => {
+  const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionsContract = await transactionsFactory.deploy();
 
- await transactions.deployed();
- 
- console.log("Transactions deployed to: ", transactions.address);
-}
+  await transactionsContract.deployed();
+
+  console.log("Transactions address: ", transactionsContract.address);
+};
 
 const runMain = async () => {
-  try{
+  try {
     await main();
     process.exit(0);
-  } catch(error){
+  } catch (error) {
     console.error(error);
     process.exit(1);
   }
-}
+};
 
 runMain();
