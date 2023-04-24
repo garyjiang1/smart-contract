2e188362cf2e4a30bbca7de516ae8ed63a997abe
Gary <jj3179@columbia.edu>
2023-04-24 13:20:43 -0400

    deploy test

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/bFPCBRHQMgOWaMCSweBv_l-Df9EI5RB9
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.18",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/bFPCBRHQMgOWaMCSweBv_l-Df9EI5RB9',
+      accounts: ['3fa4636c03a052366ed0e70fd94a8fad074f934dd0fdc780f9a28d311ae8b209']
+    }
+  }
+}
